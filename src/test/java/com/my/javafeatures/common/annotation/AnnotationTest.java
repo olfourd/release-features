d849639d99ddf1c44651ee1420671cdc034de485
Олег Гомза <o.gomza@logic-way.com>
2021-12-05 19:07:46 +0300

    Java
    - annotations

---
+++ src/test/java/com/my/javafeatures/common/annotation/AnnotationTest.java
@@ -0,0 +1,85 @@
+package com.my.javafeatures.common.annotation;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.ArrayUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.lang.NonNull;
+
+public class AnnotationTest {
+
+    @Test
+    void annotationProcessorTest() {
+        Person onePerson = new Person(1L, "One");
+        assertThat(AnnotationProcessor.canBeProcessed(onePerson), is(Boolean.TRUE));
+
+        Person zeroPerson = new Person(0L, "zero");
+        assertThrows(IllegalArgumentException.class, () -> AnnotationProcessor.canBeProcessed(zeroPerson));
+
+        User oneUser = new User(1L, "one");
+        assertThat(AnnotationProcessor.canBeProcessed(oneUser), is(Boolean.FALSE));
+    }
+
+
+    private static final class AnnotationProcessor {
+
+        private static final List<String> PROCESSED_ENT_NAMES = List.of("person", "user", "account");
+
+        @FunctionalInterface
+        private interface NotZeroValue {
+            boolean isFieldZero(Field field, Object instance);
+        }
+
+        static boolean canBeProcessed(Object entity) {
+            if (entity == null) {
+                return false;
+            }
+
+            Class<?> entityClass = entity.getClass();
+            ProcessedEntity processedEntityAnnotation = entityClass.getAnnotation(ProcessedEntity.class);
+            if (processedEntityAnnotation != null) {
+                boolean contains = PROCESSED_ENT_NAMES.contains(processedEntityAnnotation.value());
+                checkNotZeroValues(entity);
+                return contains;
+            } else {
+                return false;
+            }
+        }
+
+        static private void checkNotZeroValues(@NonNull Object instance) {
+            Field[] entityFields = instance.getClass().getDeclaredFields();
+            if (ArrayUtils.isNotEmpty(entityFields)) {
+                List<Field> emptyRequiredFields = Arrays.stream(entityFields)
+                        .filter(field -> field.isAnnotationPresent(NotZero.class))
+                        .filter(field -> ((NotZeroValue) (fld, inst) -> {
+                            try {
+                                Object instValue = fld.get(inst);
+                                return instValue instanceof Long && ((Long) instValue == 0L);
+                            } catch (Exception exception) {
+                                System.err.println("Internal error");
+                                return false;
+                            }
+                        }).isFieldZero(field, instance))
+                        .collect(Collectors.toList());
+                if (!emptyRequiredFields.isEmpty()) {
+                    throw new IllegalArgumentException(String.format("Empty fields: %s", emptyRequiredFields));
+                }
+            }
+        }
+    }
+
+    @ProcessedEntity("person")
+    private record Person(@NotZero Long id, String name) {
+    }
+
+    @ProcessedEntity("any")
+    private record User(long id, String name) {
+
+    }
+}
---
+++ src/test/java/com/my/javafeatures/common/annotation/ProcessedEntity.java
@@ -0,0 +1,13 @@
+package com.my.javafeatures.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.RECORD_COMPONENT})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ProcessedEntity {
+
+    String value();
+}
