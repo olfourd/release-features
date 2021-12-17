23096aa01dc45b4d398e9772cc355f9fbaa540cc
Олег Гомза <o.gomza@logic-way.com>
2021-12-17 14:04:07 +0300

    https://trello.com/c/XUXYO245/27-patterns
    - вариантность в java

---
+++ java-new-features/src/test/java/com/my/javafeatures/common/Covariant.java
@@ -0,0 +1,85 @@
+package com.my.javafeatures.common;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class Covariant {
+
+    @Test
+    void some() {
+        ArrayList<Fruit> fruitList = new ArrayList<>();
+        ArrayList<Citrus> citrusList = new ArrayList<>();
+        ArrayList<Orange> orangeList = new ArrayList<>();
+
+//        инвариантность — отсутствие наследования между производными типами.
+//        invariant(fruitList); нельзя передать
+        invariant(citrusList);
+//        несмотря на то что нет проблем добавления наследника в список из-за полиморфизма
+//        citrusList.add(new Orange());
+//        метод не скомпилируется, т.к. передаваемый тип инвариантен
+//        invariant(orangeList);
+
+//      ковариантность — сохранение иерархии наследования родительских типов в дочерних типах в том же порядке.
+//        получим ошибку компиляции, т.к. передаваемый тип является ковариантным и не сохраняет информацию о родительском классе
+//        covariant(fruitList);
+        covariant(citrusList);
+//        все в порядке, т.к. передаваемый тип является дочерним и инфа по нему передается
+        covariant(orangeList);
+
+//      контрвариантность — обращение иерархии родительских типов на противоположную в дочерних типах.
+        contrvariant(fruitList);
+        contrvariant(citrusList);
+//        contrvariant(orangeList);
+
+//      массивы ковариантны todo: почему?
+        Object[] objects = new Object[] {};
+        String[] strings = new String[] {};
+        objects = strings;
+
+//        листы не ковариантны todo: почему?
+//        fruitList = citrusList;
+
+    }
+
+    private void invariant(List<Citrus> citruses) {
+
+    }
+
+    private void covariant(List<? extends Citrus> oranges) {
+        Fruit fruit = oranges.get(0);
+        Citrus citrus = oranges.get(0);
+//        не пожем прочитать потомка, т.к. хз (?) какой потомок конкретно
+//        Orange orange = oranges.get(0);
+
+//        ничего добавить нельзя
+//        oranges.add(new Citrus());
+    }
+
+    private void contrvariant(List<? super Citrus> oranges) {
+//        читать не можем. просто Object
+//        Citrus citrus = oranges.get(0);
+//        Orange orange = oranges.get(0);
+
+//        хз какой потомок конкретно
+//        oranges.add(new Fruit());
+        oranges.add(new Citrus());
+        oranges.add(new Orange());
+    }
+
+    private class Fruit {
+
+    }
+
+    private class Citrus extends Fruit {
+
+    }
+
+    private class Orange extends Citrus {
+
+    }
+
+    <T> void some(Class<T> clazz) {
+        Class<? extends Class> aClass = clazz.getClass();
+    }
+}
